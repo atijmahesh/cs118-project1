6a9d59175ace5d99b3f8114621ea6918f3d082bd
txkbir <takbirr04@gmail.com>
2025-02-15 20:35:20 -0800

    initialize send/recv bufs before starting main business logic of listen_loop

--- project/transport.cpp
+++ project/transport.cpp
@@ -4,6 +4,9 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <unordered_map>
+#include <map>
+#include <set>
 #include "consts.h"
 
 using namespace std;
@@ -14,10 +17,11 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
     packet* pkt = reinterpret_cast<packet*>(buffer);
     socklen_t addr_len = sizeof(struct sockaddr_in);
     size_t payload_size = 0;
+    uint16_t client_seq = 0, server_seq = 0;
 
     if (type == CLIENT) {
         // step 1: client sends SYN packet to server to initiate handshake
-        uint16_t client_seq = (rand() % 1000) + 1;
+        client_seq = (rand() % 1000) + 1;
         pkt->seq = htons(client_seq);
         pkt->ack = 0;
 
@@ -43,7 +47,7 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
         }
 
         // step 5: client sends ACK to server to complete handshake
-        uint16_t server_seq = ntohs(pkt->seq);
+        server_seq = ntohs(pkt->seq);
         pkt->seq = htons(client_seq + 1);
         pkt->ack = htons(server_seq + 1);
 
@@ -71,8 +75,8 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
         }
         
         // step 3: server sends SYN-ACK to client
-        uint16_t client_seq = ntohs(pkt->seq);
-        uint16_t server_seq = (rand() % 1000) + 1;
+        client_seq = ntohs(pkt->seq);
+        server_seq = (rand() % 1000) + 1;
         pkt->seq = htons(server_seq);
         pkt->ack = htons(client_seq + 1); // ACK = client_seq + 1
 
@@ -90,7 +94,31 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
         print_diag(pkt, SEND);
     }
 
-    while (true) {
+    // make socket nonblocking after handshake is completed
+    init_fd(sockfd);
+    uint16_t seq_num, ack_num;
+    if (type == CLIENT) {
+        seq_num = client_seq + 1;
+        ack_num = server_seq + 1;
+    } else {
+        seq_num = server_seq + 1;
+        ack_num = client_seq + 1;
+    }
 
+    unordered_map<uint16_t, packet> send_buf; // stores unACKed packets
+    map<uint16_t, packet> recv_buf; // stores out-of-order packets
+    uint16_t window_size = MIN_WINDOW; // window size, static for now (TODO: implement flow control)
+    
+    while (true) {
+        // receive packet from sender
+        int bytes_recvd = recvfrom(sockfd, pkt, BUF_SIZE, 0, (struct sockaddr*) addr, &addr_len);
+        if (bytes_recvd < 0) continue;
+        
+        print_diag(pkt, RECV);
+        
+        if (calc_pbit(pkt) != 0) { // parity check
+            print("Corrupt packet detected. Dropping...\n");
+            continue;
+        }
     }
 }
