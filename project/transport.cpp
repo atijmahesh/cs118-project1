cde20f956f4e476bb13ca925b2442ab0ee9fc88b
txkbir <takbirr04@gmail.com>
2025-02-15 17:35:10 -0800

    server receives client SYN and should be sending SYN-ACK back to client

--- project/transport.cpp
+++ project/transport.cpp
@@ -15,7 +15,7 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
     size_t payload_size = 0;
 
     if (type == CLIENT) {
-        // step 1: client sends SYN packet
+        // step 1: client sends SYN packet to server
         uint16_t client_seq = (rand() % 1000) + 1;
         pkt->seq = htons(client_seq);
         pkt->ack = 0;
@@ -28,12 +28,38 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
         pkt->flags = SYN | set_parity(pkt);
         pkt->unused = 0;
 
-        // send SYN packet to server (client is blocked until it receives SYN-ACK)
+        // send SYN packet to server (client is now blocked until it receives SYN-ACK)
         sendto(sockfd, pkt, sizeof(packet) + payload_size, 0, (struct sockaddr*) addr, addr_len);
         print_diag(pkt, SEND);
 
     } else if (type == SERVER) {
+        // step 2: server receives SYN packet from client
 
+        // server is blocked until it receives SYN from client
+        recvfrom(sockfd, pkt, BUF_SIZE, 0, (struct sockaddr*) addr, &addr_len);
+        print_diag(pkt, RECV);
+        if (!(pkt->flags & SYN)) {
+            print("Expected SYN packet. Dropping...\n");
+            exit(1);
+        }
+        
+        // step 3: server sends SYN-ACK to client
+        uint16_t client_seq = ntohs(pkt->seq);
+        uint16_t server_seq = (rand() % 1000) + 1;
+        pkt->seq = htons(server_seq);
+        pkt->ack = htons(client_seq + 1); // ACK = client_seq + 1
+
+        // read payload data (if available)
+        payload_size = input_p(pkt->payload, MAX_PAYLOAD); // `input_io` populates `payload` field
+        pkt->length = htons(payload_size);
+        
+        pkt->win = htons(MIN_WINDOW);
+        pkt->flags = SYN | ACK | set_parity(pkt);
+        pkt->unused = 0;
+
+        // send SYN-ACK packet to client (server is now blocked until it receives ACK)
+        sendto(sockfd, pkt, sizeof(packet) + payload_size, 0, (struct sockaddr*) addr, addr_len);
+        print_diag(pkt, SEND);
     }
     while (true) {
 
