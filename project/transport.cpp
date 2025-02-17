755bb34ae5c1263562e107fa4f8c60153fb96275
txkbir <takbirr04@gmail.com>
2025-02-16 16:07:31 -0800

    change initialiation of seq_num to client_seq+2 if type==CLIENT and give 0-seq# to empty ACKs

--- project/transport.cpp
+++ project/transport.cpp
@@ -46,6 +46,8 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
             print("Expected SYN-ACK packet. Dropping...\n");
             exit(1);
         }
+        if (ntohs(pkt->length) > 0)
+            output_p(pkt->payload, ntohs(pkt->length));
 
         // step 5: client sends ACK to server to complete handshake
         server_seq = ntohs(pkt->seq);
@@ -74,6 +76,8 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
             print("Expected SYN packet. Dropping...\n");
             exit(1);
         }
+        if (ntohs(pkt->length) > 0)
+            output_p(pkt->payload, ntohs(pkt->length));
         
         // step 3: server sends SYN-ACK to client
         client_seq = ntohs(pkt->seq);
@@ -99,14 +103,13 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
     init_fd(sockfd);
     uint16_t seq_num, ack_num;
     if (type == CLIENT) {
-        seq_num = client_seq + 1;
+        seq_num = client_seq + 2;
         ack_num = server_seq + 1;
     } else {
         seq_num = server_seq + 1;
         ack_num = client_seq + 1;
     }
 
-    cout << "[Post-handshake] Starting main loop with seq_num=" << seq_num << ", ack_num=" << ack_num << endl;
     unordered_map<uint16_t, packet> send_buf; // stores unACKed packets
     map<uint16_t, packet> recv_buf; // stores out-of-order packets
     uint16_t window_size = MIN_WINDOW; // window size, static for now (TODO: implement flow control)
@@ -115,12 +118,10 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
         // receive packet from sender
         int bytes_recvd = recvfrom(sockfd, pkt, BUF_SIZE, 0, (struct sockaddr*) addr, &addr_len);
         if (bytes_recvd > 0) {
-            cout << "[MainLoop] Received " << bytes_recvd << " bytes from socket" << endl;
             print_diag(pkt, RECV);
             if (calc_pbit(pkt) == 0) {
                 uint16_t pkt_ack = ntohs(pkt->ack);
                 if (pkt->flags & ACK) {
-                    cout << "[MainLoop] Packet has ACK=" << pkt_ack << ". Removing from send_buf." << endl;
                     auto it = send_buf.begin();
                     while (it != send_buf.end()) {
                         if (it->first < pkt_ack)
@@ -135,22 +136,19 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
                 window_size = ntohs(pkt->win);
 
                 if (pkt_len > 0) {
-                    cout << "[MainLoop] Packet has " << pkt_len << " bytes of payload." << endl;
                     if (pkt_seq == ack_num) {
-                        cout << "  -> in-order data, delivering to stdout" << endl;
                         output_p(pkt->payload, pkt_len);
                         ++ack_num;
                         while (recv_buf.find(ack_num) != recv_buf.end()) {
                             packet &buf_pkt = recv_buf[ack_num];
                             uint16_t buf_len = ntohs(buf_pkt.length);
-                            cout << "  -> delivering out-of-order packet seq=" << ack_num << " with length=" << buf_len << endl;
                             output_p(buf_pkt.payload, buf_len);
                             recv_buf.erase(ack_num);
                             ++ack_num;
                         }
 
                         packet ack_pkt = {};
-                        ack_pkt.seq = htons(seq_num);
+                        ack_pkt.seq = 0; // empty ACK packets can't have a SEQ #
                         ack_pkt.ack = htons(ack_num);
                         ack_pkt.length = 0;
                         ack_pkt.win = htons(window_size);
@@ -158,7 +156,6 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
                         ack_pkt.flags |= set_parity(&ack_pkt);
 
                         sendto(sockfd, &ack_pkt, sizeof(packet), 0, (struct sockaddr*) addr, addr_len);
-                        cout << "[MainLoop] Sent ACK (seq=" << seq_num << ", ack=" << ack_num << ")" << endl;
                         print_diag(&ack_pkt, SEND);
                     }
                     else if (pkt_seq > ack_num)
@@ -180,120 +177,9 @@ void listen_loop(int sockfd, struct sockaddr_in* addr, int type, ssize_t (*input
                 p->flags |= set_parity(p);
                 send_buf[seq_num] = *p;
                 sendto(sockfd, p, sizeof(packet) + n, 0, (struct sockaddr*) addr, addr_len);
-                cout << "[MainLoop] Sent data packet seq=" << seq_num << ", ack=" << ack_num << ", length=" << n << endl;
                 print_diag(p, SEND);
                 ++seq_num;
             }
         }
     }
 }
-
-
-/*
-void listen_loop(int sockfd, struct sockaddr_in* addr, int type,ssize_t (*input_p)(uint8_t*, size_t), void (*output_p)(uint8_t*, size_t)) {
-    uint8_t buffer[BUF_SIZE] = {0};
-    packet* pkt = reinterpret_cast<packet*>(buffer);
-    socklen_t addr_len = sizeof(struct sockaddr_in);
-    size_t payload_size = 0;
-    uint16_t client_seq = 0, server_seq = 0;
-
-    if (type == CLIENT) {
-        // step 1: client sends SYN packet to server to initiate handshake
-        client_seq = (rand() % 1000) + 1;
-        pkt->seq = htons(client_seq);
-        pkt->ack = 0;
-
-        // read payload data (if available)
-        payload_size = input_p(pkt->payload, MAX_PAYLOAD); // `input_io` populates `payload` field
-        pkt->length = htons(payload_size);
-
-        pkt->win = htons(MIN_WINDOW);
-        pkt->flags = SYN;
-        pkt->unused = 0;
-        pkt->flags |= set_parity(pkt);
-
-        // send SYN packet to server (client is now blocked until it receives SYN-ACK)
-        sendto(sockfd, pkt, sizeof(packet) + payload_size, 0, (struct sockaddr*) addr, addr_len);
-        print_diag(pkt, SEND);
-
-        // step 4: client receives SYN-ACK from server (client now unblocked)
-        recvfrom(sockfd, pkt, BUF_SIZE, 0, (struct sockaddr*) addr, &addr_len);
-        print_diag(pkt, RECV);
-        if (!(pkt->flags & SYN) || !(pkt->flags & ACK)) {
-            print("Expected SYN-ACK packet. Dropping...\n");
-            exit(1);
-        }
-
-        // step 5: client sends ACK to server to complete handshake
-        server_seq = ntohs(pkt->seq);
-        pkt->seq = htons(client_seq + 1);
-        pkt->ack = htons(server_seq + 1);
-
-        // read payload data (if available)
-        payload_size = input_p(pkt->payload, MAX_PAYLOAD);
-        pkt->length = htons(payload_size);
-
-        pkt->win = htons(MIN_WINDOW);
-        pkt->flags = ACK;
-        pkt->unused = 0;
-        pkt->flags |= set_parity(pkt);
-
-        // send final ACK packet to server (handshake complete)
-        sendto(sockfd, pkt, sizeof(packet) + payload_size, 0, (struct sockaddr*) addr, addr_len);
-        print_diag(pkt, SEND);
-    } else if (type == SERVER) {
-        // step 2: server receives SYN packet from client
-
-        // server is blocked until it receives SYN from client
-        recvfrom(sockfd, pkt, BUF_SIZE, 0, (struct sockaddr*) addr, &addr_len);
-        print_diag(pkt, RECV);
-        if (!(pkt->flags & SYN)) {
-            print("Expected SYN packet. Dropping...\n");
-            exit(1);
-        }
-        
-        // step 3: server sends SYN-ACK to client
-        client_seq = ntohs(pkt->seq);
-        server_seq = (rand() % 1000) + 1;
-        pkt->seq = htons(server_seq);
-        pkt->ack = htons(client_seq + 1); // ACK = client_seq + 1
-
-        // read payload data (if available)
-        payload_size = input_p(pkt->payload, MAX_PAYLOAD); // `input_io` populates `payload` field
-        pkt->length = htons(payload_size);
-        
-        pkt->win = htons(MIN_WINDOW);
-        pkt->flags = SYN | ACK;
-        pkt->unused = 0;
-        pkt->flags |= set_parity(pkt);
-
-        // send SYN-ACK packet to client (server is now blocked until it receives ACK)
-        sendto(sockfd, pkt, sizeof(packet) + payload_size, 0, (struct sockaddr*) addr, addr_len);
-        print_diag(pkt, SEND);
-    }
-
-    int flags = fcntl(sockfd, F_GETFL);
-    flags |= O_NONBLOCK;
-    fcntl(sockfd, F_SETFL, flags);
-
-    bool client_connected = false;
-
-    while (true) {
-        // 1. Receive data from socket
-        int bytes_recvd = recvfrom(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr*)addr, &addr_len);
-        if (type == SERVER) {
-            if (bytes_recvd <= 0 && !client_connected)
-                continue; // server waiting for a client connection
-            if (bytes_recvd > 0)
-                client_connected = true; // server received data from client
-        }
-        if (bytes_recvd > 0)
-            output_p(buffer, bytes_recvd); // write data to stdout
-        
-        // 2. Read from stdin
-        int bytes_read = input_p(buffer, BUF_SIZE);
-        if (bytes_read > 0)
-            sendto(sockfd, buffer, bytes_read, 0, (struct sockaddr*)addr, addr_len);
-    }
-}
-*/
